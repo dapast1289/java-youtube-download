73e5082d1fea2fa7686948cba3632c72ce0f9821
dapast1289 <dapast1289@gmail.com.com.tw>
2018-08-04 18:29:44 +0800

    移除controller層假資料。

--- src/main/java/com/javayoutubedownload/controller/ParserController.java
+++ src/main/java/com/javayoutubedownload/controller/ParserController.java
@@ -22,33 +22,17 @@ public class ParserController {
 	
 	@RequestMapping(value = "parserUrl", method = RequestMethod.GET)
     public Map<String, Object> parser(String url) throws MalformedURLException {
-		url = "https://www.youtube.com/watch?v=wrsQBN0eyI0";
-
+		
         YouTubeInfo info = new YouTubeInfo(new URL(url));
-
         YouTubeParser parser = new YouTubeParser();
 
         List<VideoDownload> list = parser.extractLinks(info);
 
-        System.out.println("=================");
         for (VideoDownload d : list) {
             System.out.println(d.stream + ":  " + d.url);
         }
         
-        
-        
-        
 		Map<String, Object> res = new HashMap<String, Object>();
-//		ArrayList<YoutubeUrl> list = new ArrayList<YoutubeUrl>();
-//		YoutubeUrl a = new YoutubeUrl();
-//		a.setName("480P");
-//		a.setUrl(url);
-//		list.add(a);
-//		a = new YoutubeUrl();
-//		a.setUrl(url);
-//		a.setName("720P");
-//		list.add(a);
-		
 		res.put("success", true);
 		res.put("datas", list);
         return res;
