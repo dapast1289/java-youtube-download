a881d9e95e94a240a66d50f2d425fe54eb08acc8
dapast1289 <dapast1289@gmail.com.com.tw>
2018-08-04 15:50:42 +0800

    copy function from vget

---
+++ src/main/java/com/javayoutubedownload/vget/vhs/VimeoInfo.java
@@ -0,0 +1,28 @@
+package com.javayoutubedownload.vget.vhs;
+
+import java.net.URL;
+
+import com.javayoutubedownload.vget.info.VideoInfo;
+
+public class VimeoInfo extends VideoInfo {
+
+    // keep it in order hi->lo
+    public enum VimeoQuality {
+        pHi, pLow
+    }
+
+    private VimeoQuality vq;
+
+    public VimeoInfo(URL web) {
+        super(web);
+    }
+
+    public VimeoQuality getVideoQuality() {
+        return vq;
+    }
+
+    public void setVideoQuality(VimeoQuality vq) {
+        this.vq = vq;
+    }
+
+}
---
+++ src/main/java/test/BarWorker.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class BarWorker implements Runnable {
+
+	private static boolean exists = false;
+	
+	private String name;
+	
+	public BarWorker(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public void run() {
+		if (!exists) {
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (Exception e) {
+				// do nothing
+			}
+			exists = true;
+			System.out.println(name + " enter");
+			try {
+				System.out.println(name + " working");
+				TimeUnit.SECONDS.sleep(2);
+			} catch (Exception e) {
+				// do nothing
+			}
+			System.out.println(name + " leave");
+			exists = false;
+		} else {
+			System.out.println(name + " give up");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		BarWorker bar1 = new BarWorker("bar1");
+		BarWorker bar2 = new BarWorker("bar2");
+		new Thread(bar1).start();
+		new Thread(bar2).start();
+		
+	}
+
+}
